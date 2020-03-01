5830d0002d26a50a76e9c012ec3235d744653395
Pankaj Rawat <pankajrawat.333@gmail.com>
2020-03-01 14:03:51 +0530

    update main file

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from main.ota_updater import OTAUpdater
 
 
 def download_and_install_update_if_available():
+    OTAUpdater.using_network('MI','dehradun')
     ota_updater = OTAUpdater('https://github.com/PankajRawat333/esp32_ota')
-    ota_updater.download_and_install_update_if_available('MI', 'dehradun')
+    ota_updater.check_for_update_to_install_during_next_reboot()
+    ota_updater.download_and_install_update_if_available('MI','dehradun')
 
 def start():
     from main.led import Led
@@ -13,6 +15,5 @@ def start():
 def boot():
     download_and_install_update_if_available()
     start()
-
 
 boot()
